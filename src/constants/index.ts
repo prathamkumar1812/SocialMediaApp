2ede39428679b04b49c0953c3ab5c592ee7b2dd3
prathamkumar1812 <prathamkumar.mbd@gmail.com>
2023-12-09 18:09:32 +0530

    make

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,48 +1,48 @@
  export const sidebarLink=[
     {
-        imgURL:'../../public/assets/icons/home.svg',
+        imgURL:'/assets/icons/home.svg',
         route:'/',
         label:"Home"
     },
     {
-        imgURL:'../../public/assets/icons/wallpaper.svg',
+        imgURL:'/assets/icons/wallpaper.svg',
         route:'/explore',
         label:"Explore"
     },
     {
-        imgURL:'../../public/assets/icons/people.svg',
+        imgURL:'/assets/icons/people.svg',
         route: "/all-users",
         label: "People",
     },
     {
-        imgURL: "../../public/assets/icons/bookmark.svg",
+        imgURL: "/assets/icons/bookmark.svg",
         route: "/saved",
         label: "Saved",
       },
     {
-        imgURL:'../../public/assets/icons/add-post.svg',
+        imgURL:'/assets/icons/add-post.svg',
         route: "/create-post",
     label: "Create Post",
     }
 ]
 export const bottomLink=[
     {
-        imgURL:'../../public/assets/icons/home.svg',
+        imgURL:'/assets/icons/home.svg',
         route:'/',
         label:"Home"
     },
     {
-        imgURL:'../../public/assets/icons/wallpaper.svg',
+        imgURL:'/assets/icons/wallpaper.svg',
         route: "/explore",
         label: "Explore",
     },
     {
-        imgURL:'../../public/assets/icons/bookmark.svg',
+        imgURL:'/assets/icons/bookmark.svg',
         route: "/saved",
     label: "Saved",
     },
     {
-        imgURL:'../../public/assets/icons/add-post.svg',
+        imgURL:'/assets/icons/add-post.svg',
         route: "/create-post",
         label: "Create",
     }
